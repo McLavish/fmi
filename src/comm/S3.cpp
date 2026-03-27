54a127540272f41f675ca9a32492ad9c534d21ee
McLavish <Luca27ita@gmail.com>
2026-03-27 18:16:12 +0100

    fix: aws sdk api changes broke s3 file

--- src/comm/S3.cpp
+++ src/comm/S3.cpp
@@ -16,7 +16,7 @@ FMI::Comm::S3::S3(std::map<std::string, std::string> params, std::map<std::strin
     }
     instances++;
     bucket_name = params["bucket_name"];
-    Aws::Client::ClientConfiguration config;
+    Aws::S3::S3ClientConfiguration config;
     config.region = params["s3_region"];
     bandwidth = std::stod(model_params["bandwidth"]);
     overhead = std::stod(model_params["overhead"]);
@@ -25,7 +25,13 @@ FMI::Comm::S3::S3(std::map<std::string, std::string> params, std::map<std::strin
     upload_price = std::stod(model_params["upload_price"]);
 
     auto credentialsProvider = Aws::MakeShared<Aws::Auth::EnvironmentAWSCredentialsProvider>(TAG);
-    client = Aws::MakeUnique<Aws::S3::S3Client>(TAG, credentialsProvider, config);
+    // aws-sdk-cpp 1.11 expects the S3-specific configuration type and endpoint-provider signature.
+    client = Aws::MakeUnique<Aws::S3::S3Client>(
+        TAG,
+        credentialsProvider,
+        nullptr,
+        config
+    );
 }
 
 FMI::Comm::S3::~S3() {
@@ -101,4 +107,3 @@ double FMI::Comm::S3::get_price(Utils::peer_num producer, Utils::peer_num consum
     double download_costs = producer * consumer * expected_polls * download_price + producer * consumer * ((double) size_in_bytes / 1000000000.) * transfer_price;
     return upload_costs + download_costs;
 }
-
